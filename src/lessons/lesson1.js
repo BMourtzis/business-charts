6d3e42f8737189cf58a9176ea9472df01ad322df
Vasilis Mourtzis <vasilis.mourtzis@outlook.com>
2025-10-28 15:21:24 +0200

    Finished lesson 1

--- src/lessons/lesson1.js
+++ src/lessons/lesson1.js
@@ -1,10 +1,10 @@
-//Programming basics
+//Syntax basics
 function lesson() {
     //THIS IS A COMMENT.
     //We use this to write sentences that are not used by the program, but for people to read
 
     //These are constant fields, meaning they are can only by assigned once. If you try to change their values the program won't run
-    const helloGreeting = "Hello";
+    const helloGreeting = "Hello, ";
     const iAmGreeting = "I am";
     const yearsGreeting = "years old";
 
@@ -30,7 +30,8 @@ function lesson() {
         name: name,
         yearsOld: yearsOld,
         fromPalamas: fromPalamas,
-        secretMessage: "This was made by Bill with Love!"
+        secretMessage: "This was made by Bill with Love!",
+        displaySecretMessage: false
     };
 
     //return will return whatever I give it to the caller.
@@ -54,6 +55,8 @@ function makeLoud(text) {
     return text.toUpperCase() + "!!!";
 }
 
+//EXERCISES
+
 //return a string of your favourite food
 function exercise1() {
     return null;
@@ -64,14 +67,16 @@ function exercise2() {
     return null;
 }
 
-//return an object with your 3 most favourite locations, the locations should be named: location1, location2 and location3 
+//return an object with your 3 most favourite locations, the locations should be named: location1, location2 and location3.
+//For extra points, make your favourite locations loud.
 function exercise3() {
     return null;
 }
 
+//This defines what the caller of this file will see.
 export default {
     lesson: lesson(),
     exercise1: exercise1(),
     exercise2: exercise2(),
     exercise3: exercise3(),
-}
+};
