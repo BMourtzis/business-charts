a3bf57a5a1bd0b4729faeb5d725b04bd985ba87f
Vasilis Mourtzis <vasilis.mourtzis@outlook.com>
2025-11-14 16:55:45 +0200

    Adds lesson 3

--- src/lessons/lesson3.js
+++ src/lessons/lesson3.js
@@ -1,1 +1,190 @@
-//Control Flow
+//Control Flow (if/else, switch) and loops (while, for, for..of)
+function lesson() {
+    //Control Flow
+    //allows you to change the path of the code depending on a condition, like
+    //A condition is a comparision that leads to a true or false
+
+    const score = 4.5;
+    let message = "fail";
+    if(score > 5) {
+        message = "pass";
+    }
+
+    //in this case the condition is score > 5
+    //you can also do the following
+
+    let messageFromCondition = "fail";
+    const hasPassed = score > 5;
+    if(hasPassed) {
+        messageFromCondition = "passed";
+    }
+
+    //there is the if/else approach
+    //else, will be called if the condition in the if statement is false
+    const newScore = 5.5;
+    let newMessage = "";
+    if(newScore > 5) {
+        newMessage = "pass";
+    } else {
+        newMessage = "fail"
+    }
+    //the 3 pieces of code have the same functionality
+
+
+    //You can also have several conditions in the same statement, like
+    const totalScore = 85;
+    let grade = "";
+    if(totalScore > 90) {
+        grade = "high distinction";
+    } else if (totalScore > 85) { //basically this means if totalScore < 90 && totalScore > 85
+        grade = "distinction";
+    } else if( totalScore > 75) {
+        grade = "credit";
+    } else if(totalScore > 50) {
+        grade = "pass"
+    } else {
+        grade = "fail";
+    }
+    //Careful if the conditions where in a differente order, it wouldn't work the same way
+    //Like if the first condition was totalScore > 50, then that would pass or the else would pass.
+
+    //Switch is a similar control flow statement but effectively only works with equals
+    let sealColor = "";
+    switch(grade) {
+        case "credit": //if grade == "credit"
+            sealColor = "silver"
+            break;
+        case "pass":
+            sealColor = "bronze"
+            break;
+        case "distinction": // if grade == "distinction"
+        case "high distinction": // of ir grade == 'high distinction'
+            sealColor = "gold"
+            break;
+        default: // like an else statement
+            sealColor = "read"
+    }
+
+    //Loops
+    //Allows for the same functionality to be used, without repeating the code
+    //while is the simplest loop there is, that takes only the condition needed to continue
+
+    let i = 0;
+    let numberCount = "";
+    while(i < 10) {
+        //The loop has some functionality
+        numberCount += i + ", "; 
+
+        //update the i
+        i++; // meaning i = i + 1
+    }
+
+    //Updating the i is very important, as we don't it will result in an infinite loop, meaning that the loop will run forever
+
+    //There is another way to create this loop, but it's a bit more dangerous
+    let j = 0;
+    let newNumberCount = "";
+    // eslint-disable-next-line no-constant-condition
+    while(true) { //our condition is always true
+        //The loop has some functionality
+        newNumberCount += j + ", "; 
+
+        j++;
+        //Basically this is the condition we had in the while above
+        if(j >=10) {
+            break; //This tell the program to exit the loop
+        }
+    }
+
+    //There is a simpler way to write the loops above, with a for loop
+    let numberCountString = "";
+    
+    //inside the parenthesis, we do 3 things, 
+    // first we create our control variable x;
+    // then we state the condition that the loop will run for
+    // lastly, we tell it what it will do that the end of the loop
+    for(let x = 0; x < 10; x++) {
+        numberCountString += x + ", "; 
+    }
+
+    //The 3 loops above, have the same functionality
+
+    //Now, the most common usage for loops is arrays.
+    //This is an array of strings
+    var colours = [ "red", "green", "blue"];
+
+    //to access values inside the array, you have to do this.
+    colours[0]; //red
+    colours[1]; //green
+    colours[2]; //blue
+
+    //to access all 3 and do something witht them you can use a loop
+    const listLength = colours.length;
+    let colourString = "";
+    for(let idx = 0; idx < listLength; idx++) {
+        colourString += colours[idx] + ", ";
+    }
+
+    //there is also a different loop that allows us to access each item in a list
+
+    let newColourString = ""
+    for(let item of colours) { //basically item = colours[idx]
+        newColourString += item + ", ";
+    }
+
+    return {
+        newColourString, colourString, numberCountString, newNumberCount, message, messageFromCondition, newMessage, sealColor, numberCount, grade
+    }
+}
+
+//print the grade of some exams
+// > 50 pass
+// else fail
+function exercise1(grade) {
+    return "fail";
+}
+
+//I want to grade car milage
+// if milage < 5000 new
+// if milage < 10000 almost new
+// if milage < 20000 good
+// if milage < 60000 used
+// if milage < 100000 old
+// if milage < 200000 very old
+// else, how is it still working
+function exercise2(carMilage) {
+    return "how is it still working";
+}
+
+//print numbers from 1 to 100 on a string
+//bonus, do not have an comma (,) at the end
+function exercise3() {
+    return "1 2 3";
+}
+
+const acceptedColours = ["red", "blue", "green", "purple", "brown", "yellow"];
+
+//I have a list of accepted colours, return a true or false if the colour is accepted
+function exercise4(colour) {
+    //check if colour in an accepted colour
+
+    return false;
+}
+
+const wordList = ["numberBeingEqual", "negative", "text", "a", "ltE", "notNotEqual", "numberNotBeingEqual", "equalStringEqual"];
+//Find and return the longest word from the wordList
+function exercise5() {
+    return "a";
+}
+
+
+
+
+export default {
+    lesson: lesson(),
+    exercise1,
+    exercise2,
+    exercise3: exercise3(),
+    exercise4,
+    exercise5: exercise5()
+}
--- src/presentation/views/lesson/LessonThree.vue
+++ src/presentation/views/lesson/LessonThree.vue
@@ -1,49 +1,76 @@
 <template>
   <div>
-    <h1>Lesson 1</h1>
-    <p>{{ lesson.helloGreeting }} {{ lesson.name }}</p>
-    <p>{{ lesson.iAmGreeting }} {{ lesson.yearsOld }} {{ lesson.yearsGreeting }}</p>
-    <p v-if="lesson.fromPalamas">
-      {{ lesson.fromGreeting }}
-    </p>
-    <p v-if="lesson.displaySecretMessage">
-      {{ lesson.secretMessage }}
-    </p>
+    <h1>Lesson 3</h1>
+    <h3>Control Flow</h3>
+    <p>if score > 5 then message = <strong>{{ lesson.message }}</strong></p>
+    <p>if hasPassed then messageFromCondition = <strong>{{ lesson.messageFromCondition }}</strong></p>
+    <p>if newScore > 5 then newMessage = <strong>{{ lesson.newMessage }}</strong></p>
+
+    <p>total score is 85, then the grade is <strong>{{ lesson.grade }}</strong></p>
+    <p>the grade is <strong>{{ lesson.grade }}</strong> then the degree's seal color is <strong>{{ lesson.sealColor }}</strong></p>
+    <h3>Loops</h3>
+    <p>from the while(i &lt; 10) loop the result is {{ lesson.numberCount }}</p>
+    <p>from the while(true) loop the result is {{ lesson.newNumberCount }}</p>
+    <p>from the for loop the result is {{ lesson.numberCountString }}</p>
+    <p>We can loop arrays</p>
+    <p>from the for loop of colours the reuslt is {{ lesson.colourString }}</p>
+    <p>from the for..of loop of colours the reuslt is {{ lesson.newColourString }}</p>
     <h2>Exercise 1</h2>
-    <p v-if="exercise1">
-      {{ exercise1 }}
-    </p>
-    <p v-if="!exercise1">
-      No answer found
+    <p v-for="item in listOfGrade" :key="item">
+      <strong>{{ item.name }}</strong> got a grade of <strong>{{ item.grade }}</strong>, their result is <strong>{{ lessonData.exercise1(item.grade) }}</strong>
     </p>
     <h2>Exercise 2</h2>
-    <p v-if="exercise2">
-      {{ exercise2 }}
-    </p>
-    <p v-if="!exercise2">
-      No answer found
+    <p v-for="car in listOfMilage" :key="car">
+      The <strong>{{ car.name }}</strong> has <strong>{{ car.milage }}</strong> KMs, it is in <strong>{{ lessonData.exercise2(car.milage) }}</strong> condition
     </p>
     <h2>Exercise 3</h2>
-    <p v-if="exercise3">
-      <ol>
-        <li>{{ exercise3.location1 }}</li>
-        <li>{{ exercise3.location2 }}</li>
-        <li>{{ exercise3.location3 }}</li>
-      </ol>
-    </p>
-    <p v-if="!exercise3">
-      No answer found
-    </p>
+    <p>Here is the count from 1 to 100: {{ lessonData.exercise3 }}</p>
+    <h2>Exercise 4</h2>
+    <v-text-field 
+      v-model="colour"
+    />
+    <p>{{ colour }} is {{ lessonData.exercise4(colour) ? "valid" : "not valid"}}</p>
+    <h2>Exercise 5</h2>
+    <p>The longest word in my list is: <strong>{{ lessonData.exercise5 }}</strong></p>
   </div>
 </template>
 <script setup>
-import { reactive } from 'vue';
-import lessonData from '@/lessons/lesson1.js';
+import { reactive, ref } from 'vue';
+import lessonData from '@/lessons/lesson3.js';
+
+const listOfGrade = [
+  { name: "Alice", grade: 10 },
+  { name: "Ben", grade: 20 },
+  { name: "Clara", grade: 45 },
+  { name: "Dimitris", grade: 49 },
+  { name: "Eva", grade: 50 },
+  { name: "Felix", grade: 99 },
+  { name: "Georgia", grade: 85 },
+  { name: "Hassan", grade: 86 },
+  { name: "Irene", grade: 67 },
+  { name: "Jonas", grade: 77 },
+  { name: "Katerina", grade: 65 }
+];
+
+const listOfMilage = [
+  { name: "Toyota Corolla", milage: 284563 },
+  { name: "Honda Civic", milage: 15432 },
+  { name: "Ford Focus", milage: 97812 },
+  { name: "Volkswagen Golf", milage: 299001 },
+  { name: "BMW 3 Series", milage: 12456 },
+  { name: "Mercedes A-Class", milage: 186732 },
+  { name: "Audi A4", milage: 236541 },
+  { name: "Nissan Qashqai", milage: 34100 },
+  { name: "Kia Sportage", milage: 227804 },
+  { name: "Hyundai i30", milage: 19988 },
+  { name: "Peugeot 208", milage: 151234 },
+  { name: "Renault Clio", milage: 68001 }
+];
+
+const colour = ref("");
+
 
 const lesson = reactive(lessonData.lesson);
-const exercise1 = reactive(lessonData.exercise1);
-const exercise2 = reactive(lessonData.exercise2);
-const exercise3 = reactive(lessonData.exercise3);
 
 </script>
 <style scoped></style>
