ac1ba4e12d13be27fbf34000af8a2f2fbba3b50e
Vasilis Mourtzis <vasilis.mourtzis@outlook.com>
2025-11-17 13:36:00 +0200

    Adds carriers to initialize load

--- src/application/startup/initApp.ts
+++ src/application/startup/initApp.ts
@@ -1,8 +1,10 @@
+import { LoadDeliveryCarriersCommandHandler } from "../commands/deliveryCarrier/loadDeliveryCarriersCommand";
 import { LoadPartnersCommandHandler } from "../commands/partner/loadPartnersCommand";
 
 
 export async function initApplication() {
     await Promise.all([
-        new LoadPartnersCommandHandler().handle()
+        new LoadPartnersCommandHandler().handle(),
+        new LoadDeliveryCarriersCommandHandler().handle()
     ]);
 }
