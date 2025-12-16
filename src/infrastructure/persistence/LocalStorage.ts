182ad3e6cb6b1bf4ff56b2942a3e9ce1803994aa
Vasileios Mourtzis <vasilis.mourtzis@outlook.com>
2025-12-16 11:55:25 +0200

    Minor changes

--- src/infrastructure/persistence/LocalStorage.ts
+++ src/infrastructure/persistence/LocalStorage.ts
@@ -1,13 +1,16 @@
 import { IStorage } from "./type";
 
-export const LocalStorage: IStorage = {
+export const LocalStorage = class implements IStorage {
     async getItem(key: string): Promise<string | null> {
         return localStorage.getItem(key);
-    },
+    }
     async setItem(key: string, value: string): Promise<void> {
         return localStorage.setItem(key, value);
-    },
+    }
+
     removeItem(key: string): void {
         return localStorage.removeItem(key) 
     }
 }
+
+export const localStorageInstance = new LocalStorage();
