d377f69f82fa056fda001543d52f626d262d6453
Vasileios Mourtzis <vasilis.mourtzis@outlook.com>
2025-12-17 15:14:50 +0200

    Fixes reset issue for forms with arrays. adds localization

--- src/presentation/components/orders/AddOrderModal.vue
+++ src/presentation/components/orders/AddOrderModal.vue
@@ -1,71 +1,114 @@
 <template>
   <v-dialog 
     v-model="dialog" 
-    max-width="500"
+    max-width="1200"
   >
     <template #activator="{ props: activatorProps }">
       <v-btn
         v-bind="activatorProps"
         color="surface-variant"
-        :text="tCap('partner.addSupplierTitle')"
+        :text="tCap('order.addOrderTitle')"
         prepend-icon="mdi-plus"
         variant="flat"
       />
     </template>
-    <v-card :title="tCap('partner.addSupplierTitle')">
+    <v-card :title="tCap('order.addOrderTitle')">
       <v-card-text>
         <v-form 
           ref="formRef" 
           v-model="validForm"
         >
-          <v-container>
-            <v-row>
-              <v-autocomplete
-                v-model="form.partnerId"
-                :item-props="partnersToItemProps"
-                label="Partner"
-                :items="partners"
-              />
+          <v-container fluid>
+            <v-row dense>
+              <v-col cols="12">
+                <v-autocomplete
+                  v-model="form.partnerId"
+                  :item-props="partnersToItemProps"
+                  :label="tCap('partner.partner')"
+                  :items="partners"
+                  :rules="[required]"
+                />
+              </v-col>
+              <v-col cols="12">
+                <v-select
+                  v-model="form.direction"
+                  :label="tCap('order.direction')"
+                  :items="directions"
+                  :rules="[required]"
+                />
+              </v-col>
+              <v-col cols="12" class="d-flex justify-space-between align-center mb-4">
+                <h3>{{ tCap('order.items') }}</h3>
+                <v-btn
+                  color="indigo"
+                  :text="tCap('order.addOrderItemTitle')"
+                  prepend-icon="mdi-plus"
+                  variant="text"
+                  @click="addItem"
+                />
+              </v-col>
             </v-row>
-            <v-row>
-              <v-select
-                v-model="form.direction"
-                label="Direction"
-                :items="directions"
-              />
+            <v-row 
+              v-for="(orderItem, index) in form.items"
+              dense
+            >
+              <v-col cols="5">
+                <v-text-field
+                  v-model="orderItem.name"
+                  :label="tCap('order.product')"
+                  :rules="[required, maxLength(50)]"
+                />
+              </v-col>
+              <v-col cols="2">
+                <v-text-field
+                  v-model="orderItem.basePrice"
+                  :label="tCap('order.basePrice')"
+                  :rules="[required, numeric]"
+                  min="0"
+                  step="0.01"
+                  suffix="€"
+                />
+              </v-col>
+              <v-col cols="1">
+                <v-text-field
+                  v-model="orderItem.quantity"
+                  :label="tCap('order.quantity')"
+                  :rules="[required, numeric]"
+                  min="0"
+                  step="1"
+                />
+              </v-col>
+              <v-col cols="1">
+                <v-text-field
+                  :model-value="orderItem.vatRate * 100"
+                  @update:model-value="val => orderItem.vatRate = Number(val) / 100"
+                  type="number"
+                  :label="tCap('order.vatRate')"
+                  :rules="[required, numeric]"
+                  min="0"
+                  max="100"
+                  step="1"
+                  suffix="%"
+                />
+              </v-col>
+              <v-col cols="2">
+                <v-text-field
+                  readonly
+                  :value="lineAmount(orderItem)"
+                  :label="tCap('order.total')"
+                  suffix="€"
+                />
+              </v-col>
+              <v-col cols="1" class="button-alignement" align-self="center">
+                <v-btn
+                  class="justify-end"
+                  icon="mdi-trash-can"
+                  color="error"
+                  variant="text"
+                  @click="removeItem(orderItem.id)"
+                />
+              </v-col>
             </v-row>
-            <!-- <v-row>
-              <v-text-field
-                v-model="form.vatNumber"
-                :label="tCap('partner.vatNumber')"
-                :rules="[numeric, rangeLength(8, 8)]"
-              />
-            </v-row>
-            <v-row>
-              <v-text-field
-                v-model="form.email"
-                :label="tCap('partner.mainEmailField')"
-                placeholder="johndoe@gmail.com"
-                type="email"
-                :rules="[emailFormat]"
-              />
-            </v-row>
-            <v-row>
-              <v-text-field
-                v-model="form.phone"
-                :label="tCap('partner.mainPhoneField')"
-                placeholder="21080212345"
-                type="tel"
-                :rules="[phoneFormat]"
-              />
-            </v-row>
-            <v-row>
-              <v-text-field
-                v-model="form.address"
-                :label="tCap('partner.mainAddressField')"
-                :rules="[maxLength(50)]"
-              />
-            </v-row> -->
             <v-alert
               v-if="errorMessage"
               type="error"
@@ -102,13 +145,21 @@
 
 <script setup lang="ts">
 import { reactive } from 'vue';
+import { v4 as uuidv4 } from "uuid";
 
 import { OrderItemDTO } from "@/application/dto/orderDTO";
 
 import { useOrders } from '@/presentation/composables/useOrders';
 import { useLocalizationHelpers } from '@/presentation/composables/useLocalization';
 import { useFormDialog } from '@/presentation/composables/useFormDialog';
-import { PartnerDTO } from '@/application/dto/partnerDTO';
+
+import { useValidationRules } from '@/presentation/composables/useValidationRules';
+
+const { 
+  maxLength, 
+  required, 
+  numeric
+} = useValidationRules();
 
 const { createDebitOrderCommand, createCreditOrderCommand, partners, partnersToItemProps } = useOrders();
 
@@ -131,6 +182,29 @@ const {
   submit
 } = useFormDialog(form);
 
+function addItem() {
+  form.items.push({
+    id: uuidv4(),
+    name: "",
+    quantity: 1,
+    basePrice: 0.00,
+    vatRate: .24
+  });
+}
+
+function lineAmount(item: OrderItemDTO) {
+  const amount = (item.basePrice * item.quantity * (1 + item.vatRate)).toFixed(2);
+  console.log(amount);
+  return (item.basePrice * item.quantity * (1 + item.vatRate)).toFixed(2);
+}
+function removeItem(id: string) {
+  const itemIndex = form.items.findIndex(e => e.id === id);
+
+  if(itemIndex == -1) return;
+
+  form.items.splice(itemIndex, 1);
+}
+
 async function saveOrder() {
   await submit(async (form) => {
     console.log(form);
@@ -145,4 +219,8 @@ async function saveOrder() {
 
 </script>
 
-<style scoped></style>
+<style scoped>
+  .button-alignement {
+    margin-bottom: 15px;
+  }
+</style>
