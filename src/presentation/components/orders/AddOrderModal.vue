59026fd20f23b9621fe108bef94b3543f73f7e50
Vasileios Mourtzis <vasilis.mourtzis@outlook.com>
2025-12-18 19:00:19 +0200

    initial version of the orderItemEdit component

--- src/presentation/components/orders/AddOrderModal.vue
+++ src/presentation/components/orders/AddOrderModal.vue
@@ -48,66 +48,14 @@
                 />
               </v-col>
             </v-row>
-            <v-row 
-              v-for="(orderItem, index) in form.items"
-              dense
-            >
-              <v-col cols="5">
-                <v-text-field
-                  v-model="orderItem.name"
-                  :label="tCap('order.product')"
-                  :rules="[required, maxLength(50)]"
+            <v-row dense>
+              <v-expansion-panels >
+                <order-item-edit 
+                  v-for="(orderItem, index) in form.items"
+                  :key="orderItem.id"
+                  :item="orderItem"
                 />
-              </v-col>
-              <v-col cols="2">
-                <v-text-field
-                  v-model="orderItem.basePrice"
-                  :label="tCap('order.basePrice')"
-                  :rules="[required, numeric]"
-                  min="0"
-                  step="0.01"
-                  suffix="€"
-                />
-              </v-col>
-              <v-col cols="1">
-                <v-text-field
-                  v-model="orderItem.quantity"
-                  :label="tCap('order.quantity')"
-                  :rules="[required, numeric]"
-                  min="0"
-                  step="1"
-                />
-              </v-col>
-              <v-col cols="1">
-                <v-text-field
-                  :model-value="orderItem.vatRate * 100"
-                  @update:model-value="val => orderItem.vatRate = Number(val) / 100"
-                  type="number"
-                  :label="tCap('order.vatRate')"
-                  :rules="[required, numeric]"
-                  min="0"
-                  max="100"
-                  step="1"
-                  suffix="%"
-                />
-              </v-col>
-              <v-col cols="2">
-                <v-text-field
-                  readonly
-                  :value="lineAmount(orderItem)"
-                  :label="tCap('order.total')"
-                  suffix="€"
-                />
-              </v-col>
-              <v-col cols="1" class="button-alignement" align-self="center">
-                <v-btn
-                  class="justify-end"
-                  icon="mdi-trash-can"
-                  color="error"
-                  variant="text"
-                  @click="removeItem(orderItem.id)"
-                />
-              </v-col>
+              </v-expansion-panels>
             </v-row>
             <v-alert
               v-if="errorMessage"
@@ -152,8 +100,9 @@ import { OrderItemDTO } from "@/application/dto/orderDTO";
 import { useOrders } from '@/presentation/composables/useOrders';
 import { useLocalizationHelpers } from '@/presentation/composables/useLocalization';
 import { useFormDialog } from '@/presentation/composables/useFormDialog';
-
 import { useValidationRules } from '@/presentation/composables/useValidationRules';
+
+import OrderItemEdit from './OrderItemEdit.vue';
 
 const { 
   maxLength, 
@@ -186,15 +135,16 @@ function addItem() {
   form.items.push({
     id: uuidv4(),
     name: "",
-    quantity: 1,
+    // quantity: 1,
     basePrice: 0.00,
-    vatRate: .24
+    vatRate: .24,
+    variations: []
   });
 }
 
 function lineAmount(item: OrderItemDTO) {
-  const amount = (item.basePrice * item.quantity * (1 + item.vatRate)).toFixed(2);
-  return (item.basePrice * item.quantity * (1 + item.vatRate)).toFixed(2);
+  const amount = (item.basePrice  * (1 + item.vatRate)).toFixed(2);
+  return (item.basePrice * (1 + item.vatRate)).toFixed(2);
 }
 
 function removeItem(id: string) {
---
+++ src/presentation/components/orders/OrderItemEdit.vue
@@ -0,0 +1,192 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-title hide-actions v-slot="{ expanded }">
+      <v-row no-gutters>
+        <v-col class="d-flex justify-start" cols="4">
+          {{ item.name || "no name" }}
+        </v-col>
+        <v-col
+          class="text--secondary"
+          cols="8"
+        >
+          <v-fade-transition leave-absolute>
+            <v-row
+              style="width: 100%"
+              no-gutters
+            >
+              <!-- <v-col class="d-flex justify-start" cols="6">
+                Start date: {{ trip.start || 'Not set' }}
+              </v-col> -->
+              <v-col 
+                cols="3"
+              >
+                Sum: {{ lineAmount }}€
+              </v-col>
+              <v-col 
+                cols="3"
+              >
+                Total Quantity: {{ totalQuantity }}
+              </v-col>
+              <v-spacer/>
+              <v-col cols="1" >
+                <v-btn
+                  size="medium"
+                  icon="mdi-trash-can"
+                  color="error"
+                  variant="text"
+                  @click.stop="removeItem(item.id)"
+                />
+              </v-col>
+            </v-row>
+          </v-fade-transition>
+        </v-col>
+      </v-row>
+    </v-expansion-panel-title>
+    <v-expansion-panel-text>
+      <v-row
+        justify="space-around"
+        no-gutters
+      >
+        <v-col cols="6">
+          <v-text-field
+            v-model="item.name"
+            :label="tCap('order.product')"
+            :rules="[required, maxLength(50)]"
+          />
+        </v-col>
+        <v-col cols="2">
+          <v-text-field
+            v-model="item.basePrice"
+            :label="tCap('order.basePrice')"
+            :rules="[required, numeric]"
+            type="number"
+            min="0"
+            step="0.01"
+            suffix="€"
+          />
+        </v-col>
+        <v-col cols="1">
+          <v-text-field
+            :model-value="item.vatRate * 100"
+            @update:model-value="val => item.vatRate = Number(val) / 100"
+            type="number"
+            :label="tCap('order.vatRate')"
+            :rules="[required, numeric]"
+            min="0"
+            max="100"
+            step="1"
+            suffix="%"
+          />
+        </v-col>
+        <v-col cols="2">
+          <v-btn
+            color="success"
+            text="Add Variation"
+            prepend-icon="mdi-plus"
+            variant="text"
+            @click="addVariation"
+          />
+        </v-col>
+      </v-row>
+
+      <v-row 
+        v-for="(variation, index) in item.variations"
+        dense
+      >
+      <v-row dense>
+        <v-col cols="1" >
+          <v-btn
+            size="medium"
+            icon="mdi-trash-can"
+            color="error"
+            variant="text"
+            @click.stop="removeVariation(index)"
+          />
+        </v-col>
+        <v-col cols="2">
+          <v-text-field
+            v-model="variation.attributes[0]"
+            label="Colour"
+            :rules="[required, maxLength(50)]"
+          />
+        </v-col>
+        <v-col cols="2">
+          <v-text-field
+            v-model="variation.attributes[1]"
+            label="Sole"
+            :rules="[required, maxLength(50)]"
+          />
+        </v-col>
+      </v-row>
+      <v-row dense>
+          <!-- <v-col cols="1"> -->
+            <v-text-field
+              v-for="size in sizes" :key="size"
+              :label="size.toString()"
+              type="number"
+              :rules="[numeric]"
+              min="0"
+              step="1"
+            />
+          <!-- </v-col> -->
+        </v-row>
+      </v-row>
+    </v-expansion-panel-text>
+  </v-expansion-panel>
+</template>
+
+<script setup lang="ts">
+import { OrderItemDTO } from "@/application/dto/orderDTO";
+
+import { useValidationRules } from '@/presentation/composables/useValidationRules';
+import { useLocalizationHelpers } from '@/presentation/composables/useLocalization';
+import { computed, ref } from "vue";
+
+const { tCap } = useLocalizationHelpers();
+
+const { 
+  maxLength, 
+  required, 
+  numeric
+} = useValidationRules();
+
+const sizes = Array.from({ length: 47 - 35 + 1 }, (_, i) => i + 35);
+
+const props = defineProps<{
+  item: OrderItemDTO
+}>();
+
+function removeItem(id: string) {
+  //TODO: emit to parent component
+}
+
+const lineAmount = computed(() => {
+  return (props.item.basePrice * (1 + props.item.vatRate)).toFixed(2);
+})
+
+const totalQuantity = computed(() => {
+  return props.item.variations.reduce((sum, item) => sum + item.quantity, 0);
+})
+
+function addVariation() {
+  props.item.variations.push({
+    attributes: {},
+    quantity: 0
+  });
+}
+
+function removeVariation(index: number) {
+  //TODO
+}
+
+type VariationViewModel = {
+  attributes: Record<string, string>,
+  sizing: Record<string, number>
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
