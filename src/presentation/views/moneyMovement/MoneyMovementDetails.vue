48f034010287e528713cb71b606efddb248d1746
Vasileios Mourtzis <vasilis.mourtzis@outlook.com>
2026-01-29 14:26:40 +0200

    Add money movement allocations component and logic for displaying allocations

--- src/presentation/views/moneyMovement/MoneyMovementDetails.vue
+++ src/presentation/views/moneyMovement/MoneyMovementDetails.vue
@@ -1,15 +1,20 @@
 <template>
   <v-container v-if="model">
     <money-movement-details-header :money-movement="model" />
+    <v-row
+      class="mt-6"
+      align="start"
+    >
+      <v-spacer/>
+      <v-col 
+        cols="8"
+        lg="6"
+        xl="4"
+      >
+        <money-movement-details-allocations :money-movement="model" />
+      </v-col>
+    </v-row>
   </v-container>
-  <v-row
-    class="mt-6"
-    align="start"
-  >
-    <v-col cols="12">
-      <money-movement-details-allocations />
-    </v-col>
-  </v-row>
 </template>
 
 <script setup lang="ts">
